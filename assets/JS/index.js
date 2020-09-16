bdc1448ac7ae93263b736972a581108055ddf49f
Felix Smuda <f.smudo@gmail.com>
2020-09-16 15:06:54 +0200

    initial commit

---
+++ assets/JS/index.js
@@ -0,0 +1,50 @@
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$(function() {
+  // contact form animations
+  $("#contactform").click(function() {
+    $("#contactFormContainer").fadeToggle();
+  });
+  $(document).mouseup(function(e) {
+    var container = $("#contactFormContainer");
+
+    if (
+      !container.is(e.target) && // if the target of the click isn't the container...
+      container.has(e.target).length === 0
+    ) {
+      // ... nor a descendant of the container
+      container.fadeOut();
+    }
+  });
+});
